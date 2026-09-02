a73158f50c1ad7cecc58431932319821e3487e69
cherkezyan <cherkezyan@29-0>
2026-09-02 15:00:35 +0300

    Add project files.

---
+++ Project1/FileName.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+using namespace std;
+
+class Car
+{
+private:
+	string model;
+	string engine;
+	double price;
+	int sp=0;
+	string cond = "Optima";
+public:
+	void Move()
+	{ 
+		cout << "The car is moving at speed -> " << sp << "km/h\n";
+	}
+	void Start_car()
+	{
+		cout << "Start\n";
+	}
+	void Stop_car()
+	{
+		cout << "Stop\n";
+	}
+	void SpeedAdd(int a)
+	{
+		sp += a;
+	}
+	void SpeedMin(int a)
+	{
+		sp -= a;
+	}
+    
+	void Cond_On()
+	{
+		cout << cond << " ON temp=20 C\n";
+	}
+	void Cond_OFF()
+	{
+		cout << cond << " OFF\n";
+	}
+
+	void Init(string mod, string eng, double pr, string c)
+	{
+		model = mod;
+		engine = eng;
+		price = pr;
+		cond = c;
+	}
+	void PrintCar()
+	{
+		cout << "Model: " <<model<<endl;
+		cout << "Engine: " << engine << endl;
+		cout << "Price: " << price << endl;
+		cout << "Cond: " << cond << endl;
+	}
+	///  Методы-аксессоры
+	// модификатор  - инициализирует через параметр
+	void SetPrice(int p)
+	{
+		if (p > price)
+		{
+			price = p;
+		}		
+	}  
+	// инспектор - возврат значения конкретного поля
+	int GetPrice()
+	{
+		return price;
+	}
+};
+
+int main() // Client
+{
+	Car obj1;
+	obj1.Init("Audi A5","engine 1",25000,"Midea");
+	obj1.PrintCar();
+
+	obj1.SetPrice(20000);
+	obj1.PrintCar();
+
+	int price = obj1.GetPrice();
+	cout << price << endl;
+
+}
